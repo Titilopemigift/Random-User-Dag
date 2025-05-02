0b6bfd0b97d908c4d478fd2a411017a78c9e2009
Titilope Olatunji <titilopeolatunji2013@gmail.com>
2025-05-02 22:29:55 +0100

    Updated dag

--- dags/random_users_dag.py
+++ dags/random_users_dag.py
@@ -4,20 +4,19 @@ from datetime import datetime
 from utils import etl_pipeline
 
 default_args = {
-    'owner': 'titilope',
-    'retries': 1,
-    'retry_delay' : datetime.timedelta(seconds=2)
+    'owner': 'titilope'
     }
 
-with DAG(dag_id='random_user_to_s3',
+random_dag=DAG('random_user_to_s3',
          default_args=default_args,
-         description="Fetch random users and upload to S3") as dag:
+         description="Fetch random users and upload to S3") 
     
-    random_users = PythonOperator(
+random_users = PythonOperator(
         task_id='fetch_random_users',
-        python_callable=etl_pipeline
+        python_callable=etl_pipeline,
+        dag = random_dag
     )
 
     
 
-    random_users
+random_users
